2e08e0196ae5aa20403adcb77cc1d052ef34fbe9
Joshua Sullivan <jsulli0527@gmail.com>
2025-02-14 12:19:28 -0500

    Initializing the Chat Client.

---
+++ Source/UnrealChatClient/UnrealChatClient.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealChatClient.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealChatClient, "UnrealChatClient" );
+ 
---
+++ Source/UnrealChatClient/UnrealChatClientGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealChatClientGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealChatClientGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealChatClientGameMode();
+};
+
+
+
